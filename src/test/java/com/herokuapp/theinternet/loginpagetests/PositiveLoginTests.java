24d37987143eb95ff0067ffc4ab8121030e4860d
Eugene753 <Imarkadanov@gmail.com>
2022-05-12 18:19:30 -0400

    TestNG Framework

---
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLoginTests.java
@@ -0,0 +1,56 @@
+package com.herokuapp.theinternet.loginpagetests;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.LoginPage;
+import com.herokuapp.theinternet.pages.SecureAreaPage;
+import com.herokuapp.theinternet.pages.WelcomePage;
+import org.openqa.selenium.Cookie;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PositiveLoginTests extends TestUtilities {
+
+	@Test
+	public void logInTest() {
+		log.info("Starting logIn test");
+
+		// open main page
+		WelcomePage welcomePage=new WelcomePage(driver,log);
+		welcomePage.openPage();
+		takeScreenshot("WelcomePage opened");
+
+		// Click on Form Authentication link
+		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+		takeScreenshot("LoginPage opened");
+
+		// Add new cookie
+		Cookie ck=new Cookie("username","tomsmith","the-internet.herokuapp.com","/",null);
+		loginPage.setCookie(ck);
+
+		// execute log in
+		SecureAreaPage secureAreaPage = loginPage.logIn("tomsmith","SuperSecretPassword!");
+		takeScreenshot("SecureAreaPage opened");
+
+		//Get cookies
+		String username=secureAreaPage.getCookie("username");
+		log.info("Username cookie: "+username);
+		String session=secureAreaPage.getCookie("rack.session");
+		log.info("Username cookie: "+session);
+
+
+		// verifications
+		// new url
+
+		Assert.assertEquals(secureAreaPage.getCurrentUrl(), secureAreaPage.getCurrentUrl());
+
+		// log out button is visible
+		Assert.assertTrue(secureAreaPage.isLogOutButtonVisible(),"LogOut Button is not visible");
+
+		// Successful log in message
+		String expectedSuccessMessage = "You logged into a secure area!";
+		String actualSuccessMessage = secureAreaPage.getSuccessMessageText();
+		Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
+				"actualSuccessMessage does not contain expectedSuccessMessage\nexpectedSuccessMessage: "
+						+ expectedSuccessMessage + "\nactualSuccessMessage: " + actualSuccessMessage);
+	}
+}
